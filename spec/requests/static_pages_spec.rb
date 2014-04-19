488b953b513fa17c3595f99547914d96bdbb4f58
matt <mattcllhn@gmail.com>
2014-04-18 21:02:38 -0500

    finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,17 +2,46 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "Home page" do
 
+# Home page tests
+  describe "Home page" do
+it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
     it "should have the content 'Sample App'" do
-      get '/static_pages/home'
+      visit ' /static_pages/home'
       expect(page).to have_content('Sample App')
     end
   end
+# help page tests
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit ' /static_pages/help'
+      expect(page).to have_content('Help')
+    end
+   it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
+
+  end
+# About page tests
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit ' /static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+   it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+  
 end
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 
 
-RSpec.configure do |config|
-  config.include Capybara::DSL
-end
+
+
