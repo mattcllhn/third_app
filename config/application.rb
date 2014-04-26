a776afd616940a13eadfe3766a4a3afb88ecce69
matt <mattcllhn@gmail.com>
2014-04-26 16:26:42 -0500

    finish layout and routes

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+
 Bundler.require(*Rails.groups)
 
 module ThirdApp
@@ -24,5 +25,7 @@ module ThirdApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
+
